210d9d8c10e668cdbaba3e7d069bfe08fe3aad0d
dormordechai <dormordechai96@gmail.com>
2026-07-01 20:53:59 +0300

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -62,15 +62,18 @@ module.exports = {
             }
         },
         {
-          name: 'python-AI-worker',
-          script: 'python3',
-          args: 'worker.py --env-file="../.env"',
+          name: 'python-worker',
+          script: './mq/venv/bin/python',
+          args: 'worker.py --env-file=".env"',
           cwd: './mq/worker',
           watch: false,
           // Since cwd is ./mq/worker, the sentryx root .env is at ../../.env from the script's dir,
           // but parseEnvFile runs from where `pm2` command is run (the root sentryx dir).
           // So if pm2 is run in sentryx folder, parseEnvFile('./.env') finds sentryx/.env
-          env: parseEnvFile('./.env')
+          env: {
+              ...parseEnvFile('./mq/worker/.env')
+          },
+
         }
     ]
 };
